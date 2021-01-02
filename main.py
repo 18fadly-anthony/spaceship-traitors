5ba0019fd8f267d6cf45f0100eb92321d39f62d5
Anthony Fadly <anthony_@teknik.io>
2021-01-02 13:43:40 -0800

    set player names and amount of imposters

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+# Among Us Spin Off game proof of concept
+
 import sys
+
 
 def get_int(prompt):
     while True:
@@ -11,14 +14,28 @@ def get_int(prompt):
         except ValueError:
             print("Please enter a number")
 
+
 players = []
 imposters = []
 
+
 def setup_game():
+    global players
+    global imposters
+
     player_amount = get_int("Enter amount of players: ")
+
+    for i in range(player_amount):
+        players += str(input("Enter player name: "))
+
+    imposter_amount = 0
+    while imposter_amount < 1 or imposter_amount >= (player_amount / 2):
+        imposter_amount = get_int("Enter amount of imposters, it must be less than half of players: ")
+
 
 def main():
     setup_game()
+
 
 if __name__ == "__main__":
     try:
