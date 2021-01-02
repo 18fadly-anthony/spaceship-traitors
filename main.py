0d4f61d2d5450ee3ffdc5309b474a8b7b4097f5b
Anthony Fadly <anthony_@teknik.io>
2021-01-02 13:21:27 -0800

    finish get_int

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ import sys
 def get_int(prompt):
     while True:
         value = input(prompt)
-        value = int(value)
+        try:
+            value = int(value)
+            return value
+        except ValueError:
+            print("Please enter a number")
 
 players = []
 imposters = []
 
 def setup_game():
-    print("placeholder")
+    player_amount = get_int("Enter amount of players: ")
 
 def main():
-    get_int("test")
+    setup_game()
 
 if __name__ == "__main__":
     try:
